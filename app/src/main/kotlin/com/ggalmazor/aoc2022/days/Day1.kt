15b0e150710c92f2f02a95c40e559f43ba5b13f5
Guillermo Gutierrez Almazor <ggalmazor@gmail.com>
2022-12-02 10:31:36 +0100

    Refactor: reformat

--- app/src/main/kotlin/com/ggalmazor/aoc2022/days/Day1.kt
+++ app/src/main/kotlin/com/ggalmazor/aoc2022/days/Day1.kt
@@ -2,16 +2,15 @@ package com.ggalmazor.aoc2022.days
 
 class Day1 : Day {
     override fun partOne(input: String): String {
-        val elves = parseElves(input)
-
-        return elves.maxOf { it }.toString()
+        return parseElves(input).maxOf { it }
+            .toString()
     }
 
     override fun partTwo(input: String): String {
-        val elves = parseElves(input)
-
-        val top3 = elves.sortedBy { it }.reversed().take(3)
-        return top3.sum().toString()
+        return parseElves(input)
+            .sortedBy { it }.reversed()
+            .take(3).sum()
+            .toString()
     }
 
     private fun parseElves(input: String): List<Int> {
