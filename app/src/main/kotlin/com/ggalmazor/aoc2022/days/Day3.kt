2477f70b074552d6ec080b9be607e3ea4c51c634
Guillermo Gutierrez Almazor <ggalmazor@gmail.com>
2022-12-03 10:09:50 +0100

    Refactor: move private function to bottom

--- app/src/main/kotlin/com/ggalmazor/aoc2022/days/Day3.kt
+++ app/src/main/kotlin/com/ggalmazor/aoc2022/days/Day3.kt
@@ -11,18 +11,18 @@ class Day3 : Day {
         return repeatedChars.map(::priorityOf).sum().toString()
     }
 
-    private fun divideInTwo(line: String): List<String> {
-        val halfLength = line.length / 2
-        val firstHalf = line.take(halfLength)
-        val secondHalf = line.reversed().take(halfLength)
-        return listOf(firstHalf, secondHalf)
-    }
-
     override fun partTwo(input: String): String {
         val lines = input.split("\n").map(String::trim).filter(String::isNotEmpty)
         val groups = lines.windowed(3, 3)
         val repeatedChars = groups.map { group -> group.reduce(::findRepeatedChars).first() }
         return repeatedChars.map(::priorityOf).sum().toString()
+    }
+
+    private fun divideInTwo(line: String): List<String> {
+        val halfLength = line.length / 2
+        val firstHalf = line.take(halfLength)
+        val secondHalf = line.reversed().take(halfLength)
+        return listOf(firstHalf, secondHalf)
     }
 
     private fun priorityOf(char: Char): Int = if (char.isLowerCase()) char.code - 96 else char.code - 65 + 27
