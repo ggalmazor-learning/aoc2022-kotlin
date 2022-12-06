7536075fd4478cdee36933cc9b642c7d66cc877b
Guillermo Gutierrez Almazor <ggalmazor@gmail.com>
2022-12-06 17:39:06 +0000

    Refactor: extract function to parse the ranges

--- app/src/main/kotlin/com/ggalmazor/aoc2022/days/Day4.kt
+++ app/src/main/kotlin/com/ggalmazor/aoc2022/days/Day4.kt
@@ -3,25 +3,22 @@ package com.ggalmazor.aoc2022.days
 class Day4 : Day {
     override fun partOne(input: String): String {
         val lines = input.split("\n").filter { it.isNotBlank() }.map { it.trim() }
-        return lines.map { line ->
-            line.split(",").map {
-                val (start, end) = it.split("-").map(Integer::parseInt)
-                IntRange(start, end)
-            }
-        }.filter { ranges ->
+        return parseRanges(lines).filter { ranges ->
             ranges[0].subtract(ranges[1]).isEmpty() || ranges[1].subtract(ranges[0]).isEmpty()
         }.size.toString()
     }
 
     override fun partTwo(input: String): String {
         val lines = input.split("\n").filter { it.isNotBlank() }.map { it.trim() }
-        return lines.map { line ->
-            line.split(",").map {
-                val (start, end) = it.split("-").map(Integer::parseInt)
-                IntRange(start, end)
-            }
-        }.filter { ranges ->
+        return parseRanges(lines).filter { ranges ->
             ranges[0].intersect(ranges[1]).isNotEmpty()
         }.size.toString()
+    }
+
+    private fun parseRanges(lines: List<String>) = lines.map { line ->
+        line.split(",").map {
+            val (start, end) = it.split("-").map(Integer::parseInt)
+            IntRange(start, end)
+        }
     }
 }
